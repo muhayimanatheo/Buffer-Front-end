bdd638b3cf46cef5d68ee62d0bfe1c9a875a1eb1
muhayimanatheo <muhayitheo23@gmail.com>
2024-03-07 10:21:12 +0200

    being created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home";
-import ServicesFunc from "./components/services";
-import BlogFunc from "./components/blogs";
-import Parents from "./components/parent";
-import Contacts from "./components/contact";
+import Tools from "./components/Tools";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Parents />}>
-          <Route index element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Tools />} />
           <Route path="/blogs" element={<BlogFunc />} />
           <Route path="/services" element={<ServicesFunc />} />
           <Route path="/contact" element={<Contacts />} />
